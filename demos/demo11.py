407efa259c42679ca4aa29b033ea279bfe001db2
asvanthan <asvanthan@gmail.com>
2026-04-13 11:53:09 +0530

    Edited

--- demos/demo11.py
+++
@@ -1,9 +0,0 @@
-class Parent:
-    def __init__(self):
-        print("parent constaructor")
-class Child(Parent):
-    def __init__(self):
-        super().__init__()
-        print("Child Constructor")
-
-child1 =Child()
--- demos/demo13.py
+++
@@ -1,6 +0,0 @@
-def add(a =0, b =0,c =0):
-    print(a+b+c)
-
-add(2,10,5)
-add(2)
-add(5,54)
